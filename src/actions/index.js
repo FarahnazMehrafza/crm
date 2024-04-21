18638fcae55a351138ec631615652ba2e42dc0c7
Farahnaz <161342559+FarahnazMehrafza@users.noreply.github.com>
2024-04-20 20:07:51 -0400

    thirf

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,45 @@
-import { type } from "os";
+import { error } from "console";
+import { json } from "stream/consumers";
 
-export const selectperson = (peopleId) {
+export const selectperson = (peopleId) => {
    return {
       type: 'SELECTED_PERSON',
       selectId: peopleId,
    } 
 }
 
-export const noneSelected ={
+export const noneSelected = () => {
     return {
-        type:'NONE_sELETED'
+        type:'NONE_SELETED'
+    
+    }
+}
+ 
+export const createNewContact = ({ FirstName, lastName, phone, email, company, project, notes }) => {
+    return(dispatch) => {
+        fetch('http//127.0.0.1:3000/contact', {
+            method: 'POST',
+            body: JSON.string ({
+            'fisrtName' : FirstName,   
+            'lasttName' : lastName,  
+            'phone' : phone,  
+            'email' : email,
+            'company' : company,
+            'project' : project,
+            'notes' : notes,
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then (() => {
+            dispatch({ type: 'NEW_CONTACT'});
+        })
+        .then(() => {
+            dispatch(loadInitialContacts());
+        })
+        .catch(error => console.log(error))
     }
 }
 
