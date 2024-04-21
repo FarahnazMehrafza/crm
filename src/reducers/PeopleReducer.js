6beed5a6a5c2b2a0a99424ccd9293c42ed055cd8
Farahnaz <161342559+FarahnazMehrafza@users.noreply.github.com>
2024-04-20 20:39:06 -0400

    4.2

--- src/reducers/PeopleReducer.js
+++ src/reducers/PeopleReducer.js
@@ -35,6 +35,33 @@ export default (state = initialState, action) => {     //reducers here
                 personSelected: null,
             }
 
+            case 'FROM_UPDATE':
+                return{
+                    ...state,
+                    [action.payload.prop]: action.payload.value
+                }
+
+                case'NEW_CONTACT':
+                  return {
+                    ...state,
+                    fisrtName :'',   
+                    lasttName : '',  
+                    phone : '', 
+                    email : '',
+                    company : '',
+                    project : '',
+                    notes : '',
+                 }
+
+                 case 'ADD_PERSON' :
+                    return {
+                        ...state,
+                        ...action.newPerson
+                    }
+
+
+
+
         default:
             return state;    
     }
